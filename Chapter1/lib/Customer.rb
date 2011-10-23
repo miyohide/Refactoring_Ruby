9b8a16459a599fddda108f77e1766919ecbef51d
miyohide <miyohide@gmail.com>
2011-10-23 23:41:56 +0900

    create 'amount_for' method and implement statements moves 'statement' method to 'amount_for' method

--- Chapter1/lib/Customer.rb
+++ Chapter1/lib/Customer.rb
@@ -13,24 +13,29 @@ class Customer
       @rentals << arg
    end
 
+   def amount_for(element)
+      this_amount = 0
+
+      case element.movie.price_code
+      when Movie::REGULAR
+         this_amount += 2
+         this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+      when Movie::NEW_RELEASE
+         this_amount += element.days_rented * 3
+      when Movie::CHILDRENS
+         this_amount += 1.5
+         this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+      end
+
+      this_amount
+
+   end
+
    def statement
       total_amount, frequent_renter_points = 0, 0
       result = "Rental Record for #{@name}\n"
       @rentals.each do |element|
-         this_amount = 0
-
-         #determine amounts for each line
-         case element.movie.price_code
-         when Movie::REGULAR
-            this_amount += 2
-            this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-         when Movie::NEW_RELEASE
-            this_amount += element.days_rented * 3
-         when Movie::CHILDRENS
-            this_amount += 1.5
-            this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-         end
-
+         this_amount = amount_for(element)
          # add frequent renter points
          frequent_renter_points += 1
          # add bonus for a two day new release rental
@@ -40,6 +45,7 @@ class Customer
 
          # show figures for this rental
          result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+
          total_amount += this_amount
       end
 
