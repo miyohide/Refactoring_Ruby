cc3cbb436e8c13e55c2846691adbd66d3b3f38f6
miyohide <miyohide@gmail.com>
2011-11-06 22:19:46 +0900

    local variable 'frequent_renter_points' replace 'total_frequent_renter_points' method

--- Chapter1/lib/Customer.rb
+++ Chapter1/lib/Customer.rb
@@ -18,19 +18,15 @@ class Customer
    end
 
    def statement
-      frequent_renter_points = 0
       result = "Rental Record for #{@name}\n"
       @rentals.each do |element|
-         # add frequent renter points
-         frequent_renter_points += element.frequent_renter_points
-         # add bonus for a two day new release rental
          # show figures for this rental
          result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
       end
 
       # add footer lines
       result += "Amount owed is #{total_charge}\n"
-      result += "You earned #{frequent_renter_points} frequent renter points"
+      result += "You earned #{total_frequent_renter_points} frequent renter points"
       result
    end
 
@@ -39,6 +35,11 @@ class Customer
    def total_charge
       @rentals.inject(0) { |sum, rental| sum + rental.charge }
    end
+
+   def total_frequent_renter_points
+      @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
+   end
+
 
 
 end
