1a329a9666e9cb3a916cc20393baeaee4adfec79
miyohide <miyohide@gmail.com>
2014-11-02 23:37:56 +0900

    Move Method. Customer#amount_for -> Rental#charge

--- Chapter1/lib/customer.rb
+++ Chapter1/lib/customer.rb
@@ -34,19 +34,7 @@ class Customer
    end
 
    def amount_for(rental)
-      result = 0
-      # 各行の金額を計算
-      case rental.movie.price_code
-      when Movie::REGULAR
-         result += 2
-         result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-         result += rental.days_rented * 3
-      when Movie::CHILDRENS
-         result += 1.5
-         result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-      end
-      result
+      rental.charge
    end
 end
 
