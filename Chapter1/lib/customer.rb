7973961da786501dc02454e1a41f019402d8d010
miyohide <miyohide@gmail.com>
2014-11-12 11:48:17 +0900

    Delete frequent_renter_points var. Create total_frequent_renter_points method.

--- Chapter1/lib/customer.rb
+++ Chapter1/lib/customer.rb
@@ -15,21 +15,22 @@ class Customer
       result = "Rental Record for #{@name}\n"
 
       @rentals.each do |element|
-         # レンタルポイントを加算
-         frequent_renter_points += element.frequent_renter_points
-
          # このレンタルの料金を表示
          result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
       end
       # フッター行を追加
       result += "Amount owed is #{total_charge}\n"
-      result += "You earned #{frequent_renter_points} frequent renter points"
+      result += "You earned #{total_frequent_renter_points} frequent renter points"
       result
    end
 
    private
    def total_charge
       @rentals.inject(0) { |sum, rental| sum + rental.charge }
+   end
+
+   def total_frequent_renter_points
+      @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
    end
 end
 
