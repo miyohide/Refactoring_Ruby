78c2026943f15c656680cffbfc3c0577cf61964c
miyohide <miyohide@gmail.com>
2014-11-15 22:37:08 +0900

    P54. Add html_statement method.

--- Chapter1/lib/customer.rb
+++ Chapter1/lib/customer.rb
@@ -24,6 +24,20 @@ class Customer
       result
    end
 
+   def html_statement
+      result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+      @rentals.each do |element|
+         # このレンタルの料金を表示
+         result += "\t" + element.movie.title + ": " + element.charge.to_s + "<br>\n"
+      end
+
+      result += "<p>You owe <em>#{total_charge}</em><p>\n"
+      result += "On this rental you earned " +
+         "<em>#{total_frequent_renter_points}</em> " +
+         "frequent renter points<p>"
+      result
+   end
+
    private
    def total_charge
       @rentals.inject(0) { |sum, rental| sum + rental.charge }
