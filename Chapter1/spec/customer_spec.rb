9c035887f8f28b1750efb518743d3ff9613c8d47
miyohide <miyohide@gmail.com>
2014-11-15 22:37:41 +0900

    P54. Add test for html_statement method.

--- Chapter1/spec/customer_spec.rb
+++ Chapter1/spec/customer_spec.rb
@@ -73,4 +73,77 @@ describe Customer do
          it { @customer.statement.must_match /Amount owed is 20/ }
       end
    end
+
+   describe '#html_statement' do
+      describe 'REGULAR movie' do
+         describe 'Rented days is 2' do
+            before do
+               regular_movie = Movie.new('regular movie', Movie::REGULAR)
+               @customer = Customer.new('Customer name')
+               @customer.add_rental(Rental.new(regular_movie, 2))
+            end
+
+            it { @customer.html_statement.must_match /<p>You owe <em>2<\/em><p>/ }
+            it { @customer.html_statement.must_match /On this rental you earned <em>1<\/em> frequent renter points<p>/ }
+         end
+
+         describe 'Rented days is 3' do
+            before do
+               regular_movie = Movie.new('regular movie', Movie::REGULAR)
+               @customer = Customer.new('Customer name')
+               @customer.add_rental(Rental.new(regular_movie, 3))
+            end
+
+            it { @customer.html_statement.must_match /<p>You owe <em>3\.5<\/em><p>/ }
+         end
+      end
+
+      describe 'NEW RELEASE movie' do
+         before do
+            new_release_movie = Movie.new('new release movie', Movie::NEW_RELEASE)
+            @customer = Customer.new('Customer name')
+            @customer.add_rental(Rental.new(new_release_movie, 2))
+         end
+
+         it { @customer.html_statement.must_match /<p>You owe <em>6<\/em><p>/ }
+         it { @customer.html_statement.must_match /On this rental you earned <em>2<\/em> frequent renter points<p>/ }
+      end
+
+      describe 'CHILDRENS movie' do
+         describe 'Rented days is 3' do
+            before do
+               children_movie = Movie.new('children movie', Movie::CHILDRENS)
+               @customer = Customer.new('Customer name')
+               @customer.add_rental(Rental.new(children_movie, 3))
+            end
+
+            it { @customer.html_statement.must_match /<p>You owe <em>1\.5<\/em><p>/ }
+            it { @customer.html_statement.must_match /On this rental you earned <em>1<\/em> frequent renter points<p>/ }
+         end
+
+         describe 'Rented days is 4' do
+            before do
+               children_movie = Movie.new('children movie', Movie::CHILDRENS)
+               @customer = Customer.new('Customer name')
+               @customer.add_rental(Rental.new(children_movie, 4))
+            end
+
+            it { @customer.html_statement.must_match /<p>You owe <em>3\.0<\/em><p>/ }
+         end
+      end
+
+      describe 'ALL movie type' do
+         before do
+            regular_movie = Movie.new('regular movie', Movie::REGULAR)
+            new_release_movie = Movie.new('new release movie', Movie::NEW_RELEASE)
+            children_movie = Movie.new('children movie', Movie::CHILDRENS)
+            @customer = Customer.new('Customer name')
+            @customer.add_rental(Rental.new(regular_movie, 4))
+            @customer.add_rental(Rental.new(new_release_movie, 4))
+            @customer.add_rental(Rental.new(children_movie, 4))
+         end
+
+         it { @customer.html_statement.must_match /<p>You owe <em>20\.0<\/em><p>/ }
+      end
+   end
 end
