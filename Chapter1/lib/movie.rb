f4c6a784ce23d0194d9b39cb8726c1d00ffee338
miyohide <miyohide@gmail.com>
2014-11-18 23:03:55 +0900

    P63. Self Encapsulate Field for price_code.

--- Chapter1/lib/movie.rb
+++ Chapter1/lib/movie.rb
@@ -4,10 +4,14 @@ class Movie
    CHILDRENS = 2
 
    attr_reader :title
-   attr_accessor :price_code
+   attr_reader :price_code
 
-   def initialize(title, price_code)
-      @title, @price_code = title, price_code
+   def price_code=(value)
+      @price_code = value
+   end
+
+   def initialize(title, the_price_code)
+      @title, self.price_code = title, the_price_code
    end
 
    def charge(days_rented)
