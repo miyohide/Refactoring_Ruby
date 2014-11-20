584acfdd61fae4c13c96e917fddb1099702add9e
miyohide <miyohide@gmail.com>
2014-11-20 23:49:02 +0900

    P65. create new instance val(price).

--- Chapter1/lib/movie.rb
+++ Chapter1/lib/movie.rb
@@ -1,3 +1,7 @@
+require_relative './regular_price'
+require_relative './new_release_price'
+require_relative './childrens_price'
+
 class Movie
    REGULAR = 0
    NEW_RELEASE = 1
@@ -8,6 +12,14 @@ class Movie
 
    def price_code=(value)
       @price_code = value
+      @price = case price_code
+               when REGULAR
+                  RegularPrice.new
+               when NEW_RELEASE
+                  NewReleasePrice.new
+               when CHILDRENS
+                  ChildrensPrice.new
+               end
    end
 
    def initialize(title, the_price_code)
