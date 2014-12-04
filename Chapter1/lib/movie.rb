5ffa2f230e06572973a8230f462ad855cdb70457
miyohide <miyohide@gmail.com>
2014-12-05 00:17:57 +0900

    P69. Rewrite Movie.charge method.

--- Chapter1/lib/movie.rb
+++ Chapter1/lib/movie.rb
@@ -27,18 +27,7 @@ class Movie
    end
 
    def charge(days_rented)
-      result = 0
-      # 各行の金額を計算
-      case price_code
-      when REGULAR
-         return @price.charge(days_rented)
-      when NEW_RELEASE
-         result += days_rented * 3
-      when CHILDRENS
-         result += 1.5
-         result += (days_rented - 3) * 1.5 if days_rented > 3
-      end
-      result
+      @price.charge(days_rented)
    end
 
    def frequent_renter_points(days_rented)
